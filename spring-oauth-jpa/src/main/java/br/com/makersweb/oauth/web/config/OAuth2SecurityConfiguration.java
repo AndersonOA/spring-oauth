0d881591c06865fa273a827e69ad34b566e9b4b6
Anderson Orione Aristides <marrecogevans@gmail.com>
2017-03-09 18:59:59 -0300

    oauth2 via banco de dados

--- spring-oauth-jpa/src/main/java/br/com/makersweb/oauth/web/config/OAuth2SecurityConfiguration.java
+++ spring-oauth-jpa/src/main/java/br/com/makersweb/oauth/web/config/OAuth2SecurityConfiguration.java
@@ -3,6 +3,8 @@
  */
 package br.com.makersweb.oauth.web.config;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,8 +14,9 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+import org.springframework.security.oauth2.provider.code.AuthorizationCodeServices;
+import org.springframework.security.oauth2.provider.code.JdbcAuthorizationCodeServices;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 
 /**
  *
@@ -25,7 +28,13 @@ import org.springframework.security.oauth2.provider.token.store.InMemoryTokenSto
 public class OAuth2SecurityConfiguration extends AuthorizationServerConfigurerAdapter {
 
 	@Autowired
+	private MyUserDetailsService userDetailsService;
+
+	@Autowired
 	private AuthenticationManager authenticationManager;
+
+	@Autowired
+	private DataSource dataSource;
 
 	@Override
 	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
@@ -34,21 +43,23 @@ public class OAuth2SecurityConfiguration extends AuthorizationServerConfigurerAd
 
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-		clients.inMemory().withClient("manager").secret("xpto")
-				.authorizedGrantTypes("password", "authorization_code", "refresh_token")
-				.authorities("ADMIN").scopes("read", "write", "trust")
-				.accessTokenValiditySeconds(120)
-				.refreshTokenValiditySeconds(600);
+		clients.jdbc(dataSource);
 	}
 
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-		endpoints.tokenStore(tokenStore()).authenticationManager(authenticationManager);
+		endpoints.userDetailsService(userDetailsService).authorizationCodeServices(authorizationCodeServices())
+				.authenticationManager(authenticationManager).tokenStore(tokenStore()).approvalStoreDisabled();
 	}
 
 	@Bean
-	public TokenStore tokenStore() {
-		return new InMemoryTokenStore();
+	public JdbcTokenStore tokenStore() {
+		return new JdbcTokenStore(dataSource);
+	}
+
+	@Bean
+	protected AuthorizationCodeServices authorizationCodeServices() {
+		return new JdbcAuthorizationCodeServices(dataSource);
 	}
 
 }
